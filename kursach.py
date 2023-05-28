4075d27eba7c338aa2023e010a4afe1c213df6f4
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2023-05-29 00:23:13 +0300

    final vers

--- kursach.py
+++ kursach.py
@@ -1,12 +1,10 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor, RandomForestClassifier, RandomForestRegressor
-from sklearn.metrics import f1_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Lasso
 
 def read_data(path):
     # Reads the file with the given delimiter ","
@@ -27,6 +25,7 @@ def check_data(data):
     # Count na
     print("\n Count na")
     print(data.isna().sum())
+
 
 def transform_runtime_to_minutes(runtime):
     if pd.isnull(runtime):
@@ -110,34 +109,37 @@ def linear_regression(X_train, X_test, y_train, y_test):
     model = LinearRegression()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
+    #score with r-squared
+    r2 = r2_score(y_test, y_pred)
+    print("Linear Regression R-squared: ", r2)
     scatter_plot(y_test, y_pred, 'Linear Regression')
-    return mse
+    return r2
 
 def random_forest_regression(X_train, X_test, y_train, y_test):
     # Random Forest regression
     model = RandomForestRegressor()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    print("Random Forest Regression R-squared: ", r2)
     scatter_plot(y_test, y_pred, 'Random Forest Regression')
-    return mse
+    return r2
 
-def gradient_boosting_regression(X_train, X_test, y_train, y_test):
-    # Gradient Boosting regression
-    model = GradientBoostingRegressor()
+def lasso_regression(X_train, X_test, y_train, y_test):
+    # Lasso regression
+    model = Lasso()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    scatter_plot(y_test, y_pred, 'Gradient Boosting Regression')
-    return mse
+    r2 = r2_score(y_test, y_pred)
+    print("Lasso Regression R-squared: ", r2)
+    scatter_plot(y_test, y_pred, 'Lasso Regression')
+    return r2
 
 def visualize_regression_results(mse_scores):
-    models = ['Linear Regression', 'Random Forest Regression', 'Gradient Boosting Regression']
-    
+    models = ['Linear Regression', 'Random Forest Regression','Lasso Regression']
     plt.bar(models, mse_scores)
     plt.xlabel('Regression Models')
-    plt.ylabel('Mean Squared Error (MSE)')
+    plt.ylabel('R-square')
     plt.title('Comparison of Regression Models')
     plt.show()
 
@@ -165,41 +167,42 @@ if __name__ == "__main__":
     data_path_language = 'data/dim_language.csv'
     dataset_language = read_data_semi(data_path_language)
 
-    
 
     dataset_rotten_tomatoes=dataset_rotten_tomatoes.drop(columns=['title','type','box_office_(gross_usa)','view_the_collection','aspect_ratio','sound_mix','release_date_(theaters)','production_co','consensus','synopsis','crew','link','director','producer','writer','release_date_(streaming)'])
-    
-    #if column runtime is nan then replace it with average
-    dataset_rotten_tomatoes['runtime'].fillna(-1, inplace=True)
-    #in column runtime transform to minutes (2h 3m = 133)
-    dataset_rotten_tomatoes['runtime'] = dataset_rotten_tomatoes['runtime'].apply(transform_runtime_to_minutes)
+
+
+
 
     dataset_rotten_tomatoes['genre'] = dataset_rotten_tomatoes['genre'].apply(change_genre, dataset_genre=dataset_genre)
     dataset_rotten_tomatoes['rating'] = dataset_rotten_tomatoes['rating'].apply(change_rating, dataset_rating=dataset_rating)
-    dataset_rotten_tomatoes['total_ratings'] = dataset_rotten_tomatoes['total_ratings'].apply(change_total_ratings)
-    # change na 
-
-    dataset_rotten_tomatoes['people_score'].fillna(dataset_rotten_tomatoes['people_score'].mean(), inplace=True)
     dataset_rotten_tomatoes['original_language'] = dataset_rotten_tomatoes['original_language'].apply(change_original_language, dataset_language=dataset_language)
-    #convert column people_score to int
+
+
+    dataset_rotten_tomatoes['total_ratings'] = dataset_rotten_tomatoes['total_ratings'].apply(change_total_ratings)
+    dataset_rotten_tomatoes['runtime'] = dataset_rotten_tomatoes['runtime'].apply(transform_runtime_to_minutes)
+
+    # change na 
+    dataset_rotten_tomatoes['runtime'].fillna(dataset_rotten_tomatoes['runtime'].mean(), inplace=True)
+    dataset_rotten_tomatoes['people_score'].fillna(dataset_rotten_tomatoes['people_score'].mean(), inplace=True)
     dataset_rotten_tomatoes['people_score'] = dataset_rotten_tomatoes['people_score'].astype(int)
-
-
     check_data(dataset_rotten_tomatoes)
 
-        # Select features and target variable
+
+    # check_data(dataset_rotten_tomatoes)
+
+    #     # Select features and target variable
     X = dataset_rotten_tomatoes[['year', 'people_score', 'total_reviews', 'total_ratings',  'runtime', 'genre', 'rating', 'original_language']]
     y = dataset_rotten_tomatoes['critic_score']
     # Split the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    mse_scores = [
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    scores = [
     linear_regression(X_train, X_test, y_train, y_test),
     random_forest_regression(X_train, X_test, y_train, y_test),
-    gradient_boosting_regression(X_train, X_test, y_train, y_test)
+    lasso_regression(X_train, X_test, y_train, y_test)
     ]
 
     # Visualize the results
     #This code will generate a bar plot comparing the mean squared error (MSE) for each regression model. The lower the MSE, the better the model's performance.
-    visualize_regression_results(mse_scores)
+    visualize_regression_results(scores)
 
 
